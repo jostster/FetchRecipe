79ef96830757ebbb28ef6bed1fb83c39f3857b0d
Brian Jost <jostster@gmail.com>
2024-10-16 20:23:42 -0500

    Initial Commit

---
+++ FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Brian Jost on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
